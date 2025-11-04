a4a4caba0411c3c3e46c6ab817815debad15290b
HenriqueRibeiroVolvocars <henrique.baldim@volvocars.com>
2025-11-04 09:44:24 -0300

    add: Settings API for works

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from backend.supabase_client import supabase
 
-# Create your views here.
+@api_view(['GET'])
+def listar_dados(request):
+    """
+    Exemplo: busca dados da tabela 'clientes' no Supabase e retorna para o frontend.
+    """
+    try:
+        data = supabase.table("inmetro_database").select("*").execute()
+        return Response(data.data)
+    except Exception as e:
+        import traceback
+        return Response({
+            "erro": str(e),
+            "trace": traceback.format_exc()
+        }, status=500)
