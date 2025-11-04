b87718d12a2019dba7ff97e48888db62e4d52d29
Henrique Ribeiro Souza <henrique.baldim@volvocars.com>
2025-11-04 14:32:46 -0300

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$((e88fxezg#pqi!kz7zo3)^zqu*^pqi^**p6is$n-tdg65jp(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://api-volvo-homecharging-dweqdpaecqc5e6f9.centralus-01.azurewebsites.net']
+ALLOWED_HOSTS = ['api-volvo-homecharging-dweqdpaecqc5e6f9.centralus-01.azurewebsites.net']
 
 
 # Application definition
